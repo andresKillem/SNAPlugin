74b260fe149c1f38d1da968f51e554859212cb96
Aday Talavera <aday.talavera@gmail.com>
2012-10-22 14:06:16 +0100

    Added admin config option to enable/disable plugin

--- lang/en/local_cicei_snatools.php
+++ lang/en/local_cicei_snatools.php
@@ -7,3 +7,5 @@ $string['pluginname'] = 'CICEI Social Network Analysis Tools';
 $string['ciceisnatools:use'] = 'Use CICEI Social Network Analysis tools';
 
 // Texts
+$string['enable_cicei_snatools'] = 'Usar CICEI Social Network Analysis Tools';
+$string['explainenable_cicei_snatools'] = 'CICEI Social Network Analysis Tools allows admins and teachers to extract data from networks inside course forums';
--- settings.php
+++ settings.php
@@ -3,7 +3,12 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) { // needs this condition or there is error on login page
-    //$settings = new admin_settingpage('local_ciceiconditional_settings', get_string('settings', 'local_ciceiconditional'));
-    //$settings->add(new admin_setting_configcheckbox('local_ciceiconditional/enableconditionals', get_string('enableconditionals','local_ciceiconditional'), get_string('explainenableconditionals','local_ciceiconditional'), 0));
-    //$ADMIN->add('localplugins', $settings);
+    $settings = new admin_settingpage('local_cicei_snatools_settings', get_string('pluginname', 'local_cicei_snatools'));
+    $settings->add(new admin_setting_configcheckbox(
+            'local_cicei_snatools/enabled',
+            get_string('enable_cicei_snatools', 'local_cicei_snatools'),
+            get_string('explainenable_cicei_snatools', 'local_cicei_snatools'),
+            0)
+    );
+    $ADMIN->add('localplugins', $settings);
 }
